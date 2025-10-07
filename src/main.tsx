3559c2e048b61d550feaebbdbf3550717c284ef3
Shay Cojocaru <shayc@outlook.com>
2025-10-08 01:01:03 +0300

    chore: wrap App component with ThemeProvider and create a custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -7,8 +8,18 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css'
 
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: '#1976d2' },
+    },
+  },
+);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
