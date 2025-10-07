2e1590b27fa0734ba2e8ef4b3d3c6f6595ed445f
Shay Cojocaru <shayc@outlook.com>
2025-10-08 01:12:08 +0300

    chore: clean up App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import App from './App.tsx'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './index.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-const theme = createTheme(
-  {
-    palette: {
-      primary: { main: '#1976d2' },
-    },
+const theme = createTheme({
+  palette: {
+    primary: { main: "#1976d2" },
   },
-);
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
