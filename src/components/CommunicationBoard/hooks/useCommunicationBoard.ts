04490ff44133a508a710f3a59ed9db219b5aa1cf
Shay Cojocaru <shayc@outlook.com>
2025-10-08 07:11:15 +0300

    feat: add camelcase-keys for deep key transformation in useCommunicationBoard

--- src/components/CommunicationBoard/hooks/useCommunicationBoard.ts
+++ src/components/CommunicationBoard/hooks/useCommunicationBoard.ts
@@ -1,7 +1,9 @@
+import camelcaseKeys from "camelcase-keys";
 import lotsOfStuff from "../../../open-board-format/examples/lots_of_stuff.json";
 import type { Board } from "../types";
 
 export function useCommunicationBoard() {
   const board = lotsOfStuff as unknown as Board;
-  return board;
+  const camelCasedBoard = camelcaseKeys(board, { deep: true });
+  return camelCasedBoard;
 }
