ab5f70dc0b6b8a377b42f1bc5b4f1ffc2dc79439
Shay Cojocaru <shayc@outlook.com>
2025-11-06 01:54:14 +0200

    refactor: simplify BackspaceButton component by removing unused state and improving code clarity

--- src/features/board/components/MessageBar/BackspaceButton.tsx
+++ src/features/board/components/MessageBar/BackspaceButton.tsx
@@ -2,35 +2,29 @@ import BackspaceIcon from "@mui/icons-material/Backspace";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import { useState } from "react";
-import { mergeProps, useLongPress, usePress } from 'react-aria';
+import { mergeProps, useLongPress, usePress } from "react-aria";
 
 interface BackspaceButtonProps {
   onPress: () => void;
   onLongPress: () => void;
 }
 
-export function BackspaceButton({ onPress, onLongPress }: BackspaceButtonProps) {
-  const [longPressStarted, setLongPressStarted] = useState(false);
-
-  let { pressProps } = usePress({
-    onPress
+export function BackspaceButton({
+  onPress,
+  onLongPress,
+}: BackspaceButtonProps) {
+  const { pressProps } = usePress({
+    onPress,
   });
 
   const { longPressProps } = useLongPress({
-    accessibilityDescription: 'Long press to clear message',
+    accessibilityDescription: "Long press to clear message",
     onLongPress,
-    onLongPressStart: () => {
-      setLongPressStarted(true);
-    },
-    onLongPressEnd: () => {
-      setLongPressStarted(false);
-    },
   });
 
   return (
     <Tooltip title="Backspace" enterDelay={800}>
-      <Box sx={{ alignSelf: "center" }}>
+      <Box sx={{ alignSelf: "center", position: "relative" }}>
         <IconButton
           {...mergeProps(pressProps, longPressProps)}
           aria-label="Backspace"
@@ -41,5 +35,5 @@ export function BackspaceButton({ onPress, onLongPress }: BackspaceButtonProps) 
         </IconButton>
       </Box>
     </Tooltip>
-  )
+  );
 }
