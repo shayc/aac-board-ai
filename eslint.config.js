7123a1f49f98ecc7dc0bfd79310a627c38bac826
Shay Cojocaru <shayc@outlook.com>
2025-11-20 00:59:04 +0200

    chore: add 'coverage' to global ignores in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "coverage"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
