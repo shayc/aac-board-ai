57835f29f05ec5c553c88f0b26e4de7e7697333a
Shay Cojocaru <shayc@outlook.com>
2025-12-02 01:33:37 +0200

    fix: refactor SettingsDrawer component structure and add individual settings components

--- src/app/shell/SettingsDrawer/SettingsDrawer.tsx
+++ src/app/shell/SettingsDrawer/SettingsDrawer.tsx
@@ -5,10 +5,10 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-import { AISettings } from "./AISettings/AISettings";
-import { AppearanceSettings } from "./AppearanceSettings/AppearanceSettings";
-import { LanguageSettings } from "./LanguageSettings/LanguageSettings";
-import { SpeechSettings } from "./SpeechSettings/SpeechSettings";
+import { AISettings } from "./components/AISettings";
+import { AppearanceSettings } from "./components/AppearanceSettings";
+import { LanguageSettings } from "./components/LanguageSettings";
+import { SpeechSettings } from "./components/SpeechSettings";
 
 export interface SettingsDrawerProps {
   open: boolean;
