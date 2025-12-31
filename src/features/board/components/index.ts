d0299d795424fd34bcaa0964d4df88dc667d2d04
Shay Cojocaru <shayc@outlook.com>
2026-01-01 00:52:36 +0200

    chore: rename NavigationButtons to NavButtons for consistency and add NavButtons component

--- src/features/board/components/index.ts
+++ src/features/board/components/index.ts
@@ -2,7 +2,7 @@ export { Board } from "./Board/Board";
 export { BoardSetSelect } from "./BoardSetSelect/BoardSetSelect";
 export { Grid } from "./Grid/Grid";
 export { MessageBar } from "./MessageBar/MessageBar";
-export { NavigationButtons } from "./NavigationButtons/NavigationButtons";
+export { NavButtons } from "./NavButtons/NavButtons";
 export { Pictogram } from "./Pictogram/Pictogram";
 export { SuggestionBar } from "./SuggestionBar/SuggestionBar";
 export { Tile } from "./Tile/Tile";
