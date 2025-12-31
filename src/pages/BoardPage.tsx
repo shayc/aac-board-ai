59318db689da093590f403e5f2c1ce9533d45287
Shay Cojocaru <shayc@outlook.com>
2026-01-01 00:14:33 +0200

    chore: wrap Board component in ErrorBoundary and add ErrorFallback component

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,5 +1,11 @@
 import { Board } from "@features/board/components/Board/Board";
+import { ErrorFallback } from "@shared/components/ErrorFallback";
+import { ErrorBoundary } from "react-error-boundary";
 
 export function BoardPage() {
-  return <Board />;
+  return (
+    <ErrorBoundary fallback={<ErrorFallback />}>
+      <Board />
+    </ErrorBoundary>
+  );
 }
