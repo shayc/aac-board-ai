d3318fc846acfae269cd597059802b223c343b94
Shay Cojocaru <shayc@outlook.com>
2026-04-14 23:16:53 +0300

    refactor: update file import path for openFiles utility and create new file-picker module

--- src/features/board/hooks/useImportBoardFiles.ts
+++ src/features/board/hooks/useImportBoardFiles.ts
@@ -1,5 +1,5 @@
 import { useSnackbar } from "@shared/snackbar/useSnackbar";
-import { openFiles } from "@shared/utils/files";
+import { openFiles } from "@shared/utils/file-picker";
 import { importBoardFiles as importFiles } from "../store/board-sets-store";
 
 export interface UseImportBoardFilesReturn {
